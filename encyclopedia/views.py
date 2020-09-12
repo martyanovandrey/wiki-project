28bacb7a715b76a78b283828591ea80158f9cfa8
Martyanov Andrey <37772440+martyanovandrey@users.noreply.github.com>
2020-09-12 12:30:27 +0300

    add search

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -18,9 +18,15 @@ def my_content(request, title):
 
 def search_wiki(request):
     query = request.GET.get('q')
-    if query:
+    entries = util.list_entries()
+    if any(query.lower() == val.lower() for val in entries):
         title_md = util.get_entry(query)
         title_html = markdown2.markdown(title_md)
         return render(request, "encyclopedia/title.html", {
             "title": title_html
         })
+    else:
+        match_list = [s for s in entries if query.lower() in s.lower()]
+        return render(request, "encyclopedia/match.html", {
+        "entries": match_list
+    })
