8f914066199cacb9da58381f82903bef843e5493
Martyanov Andrey <37772440+martyanovandrey@users.noreply.github.com>
2021-01-25 10:39:52 +0300

    css changes

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from . import util
 import markdown2
 from django import forms
-from django.http import HttpResponseRedirect, HttpResponseNotFound
+from django.http import HttpResponseRedirect, HttpResponseNotFound, HttpResponseBadRequest
 from django.urls import reverse
 from django.utils.html import strip_tags
 from django.shortcuts import redirect
@@ -21,6 +21,11 @@ def index(request):
 #Load page from .md file and convert to html.
 def my_content(request, title):
     title_md = util.get_entry(title)
+    if title_md == None:
+        return render(request, "encyclopedia/index.html", {
+            'error': 'We do not have an article on this topic yet, let\'s see what we got:',
+            "entries": util.list_entries()
+        })
     title_html = markdown2.markdown(title_md)
     title_load = strip_tags(title_html)
     return render(request, "encyclopedia/title.html", {
@@ -89,8 +94,4 @@ def edit_page(request, title):
 #Random page
 def random_page(request):
     title = random.choice(util.list_entries())
-    title_md = util.get_entry(title)
-    title_html = markdown2.markdown(title_md)
-    title_load = strip_tags(title_html)
-
     return HttpResponseRedirect('/wiki/'+title)
